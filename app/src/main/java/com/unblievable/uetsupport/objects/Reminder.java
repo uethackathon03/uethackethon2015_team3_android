7fa84fd25bf267eadda8e31e8749fa8951bdab19
DucAnh <ducanh54@gmail.com>
2015-11-20 22:39:07 +0700

    update object

---
+++ app/src/main/java/com/unblievable/uetsupport/objects/Reminder.java
@@ -0,0 +1,15 @@
+package com.unblievable.uetsupport.objects;
+
+/**
+ * Created by DucAnhZ on 20/11/2015.
+ */
+public class Reminder {
+
+    public Integer reminderId;
+    public Long timeReminder;
+    public Long beforeReminder;
+    public Integer numberOfReminder;
+    public String title;
+    public String note;
+
+}
---
+++ app/src/main/java/com/unblievable/uetsupport/objects/Teacher.java
@@ -0,0 +1,18 @@
+package com.unblievable.uetsupport.objects;
+
+/**
+ * Created by DucAnhZ on 20/11/2015.
+ */
+public class Teacher {
+
+    public Long teacherId;
+
+    public String fullname;
+    public String email;
+    public String phone;
+    public String address;
+    public String description;
+
+    public String facultyName;
+
+}
